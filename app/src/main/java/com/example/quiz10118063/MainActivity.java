055c2c3dc8dc57e8a5d5224bd9b197f6d2a1fb15
NaufalAshaW <naufal.asha@gmail.com>
2021-05-29 10:50:58 +0700

    Initial commit

---
+++ app/src/main/java/com/example/quiz10118063/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.quiz10118063;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+// Tanggal Pengerjaan : 29 Mei 2021
+// NIM                : 10118063
+// Nama               : Naufal Asha
+// Kelas              : IF-2
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
